2f3a8c96fe19819d1881fd0a4289cea6b7d41313
Ihor <u96ok69@gmail.com>
2025-10-06 20:01:01 +0300

    make gome page html and css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,28 @@
+import css from "./HomePage.module.css";
+
 export default function HomePage() {
   return (
     <section>
-      <h1>Home Page</h1>
+      <div className={css.sectionContainer}>
+        <h1 className={css.mainTitle}>Make Life Easier for the Family:</h1>
+        <h2 className={css.lowerTitle}>
+          Find Babysitters Online for All Occasions
+        </h2>
+        <button className={css.btn}>
+          Get started<svg width={18} height={0} className={css.iconArrow}></svg>
+        </button>
+        <div className={css.background}>
+          <div className={css.textContainer}>
+            <div className={css.checkCont}>
+              <svg width={30} height={30} className={css.iconCheck}></svg>
+            </div>
+            <div className={css.textLines}>
+              <p className={css.text}>Experienced nannies</p>
+              <p className={css.number}>15,000</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
